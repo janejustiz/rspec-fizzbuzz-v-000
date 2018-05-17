4515c4cdee59518f32b4212c71712aab732697ee
Jane Justiz <janejustiz@gmail.com>
2018-05-17 02:13:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-elsif int % 5 == 0 # if the number int is divisible by 3
+  elsif int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go fizz
-  if int % 3 && 5 == 0 # if the number int is divisible by 3
+  elsif int % 3 && 5 == 0 # if the number int is divisible by 3
     "FizzBuzz" # Go fizz
   end
 end
